29c7469202e4489dce6a5ab13e1ef00c2286ab7a
mayank berry <mayank.berry.5b@gmail.com>
2024-01-04 17:29:19 +0530

    ggg

--- ecom/settings.py
+++ ecom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m7@0p-55o8nsc+zi+=hf+x+(oy-&lw$57nvuqg802y(=t0mn@x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app"]
 
